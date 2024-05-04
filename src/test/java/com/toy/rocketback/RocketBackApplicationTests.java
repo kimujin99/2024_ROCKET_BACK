33893603624dd3fb48b6b6ca5cf13ca6b590ad26
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-05-04 22:34:35 +0900

    #2 - 스프링프로젝트 의존성 추가 및 패키지 구분
    프로젝트를 시작함에 있어 필요한 의존성을 추가하고, 패키지를 구분해 프로젝트를 관리한다.

---
+++ src/test/java/com/toy/rocketback/RocketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.rocketback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
