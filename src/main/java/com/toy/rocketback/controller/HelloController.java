02b81eaa70e52e01b0ec98f127a14cc4f68ab049
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-06-16 12:36:52 +0900

    build.gradle 파일 변경 및 컨트롤러 어노테이션 추가

--- src/main/java/com/toy/rocketback/controller/HelloController.java
+++ src/main/java/com/toy/rocketback/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.toy.rocketback.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World 비상탈출로켓입니다!";
     }
